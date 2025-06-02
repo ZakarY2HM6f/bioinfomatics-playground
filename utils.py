7699e9091d7e25ccb2e9f607c00a9756e1ad85ac
Zachary Y.H. Mo <zacharymono@icloud.com>
2025-06-02 08:39:05 +0800

    moved read from file to utils

--- utils.py
+++ utils.py
@@ -2,9 +2,14 @@ import collections
 
 NUCLEOTIDES = ['A', 'C', 'G', 'T']
 
-def validateSeq(seq: str):
-    seq = seq.upper()
+def validateSeq(seq: str) -> bool:
     for b in seq:
         if b not in NUCLEOTIDES:
             return False
     return True
+
+def readSeqFromFile(path: str) -> str:
+    with open(path, 'r') as f:
+        seq = f.read().strip().upper()
+        assert(validateSeq(seq))
+        return seq
